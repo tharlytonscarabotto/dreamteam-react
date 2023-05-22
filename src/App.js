5ccf8ec3bcb7294dba8f986c86a907197a6da536
tharlysc <tharlyton.scarabotto@outlook.com>
2023-05-22 15:26:56 -0300

    adjusting the last layouts

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 import FieldPositions from './components/FieldPositions';
+import './App.css';
 
 function App() {
 
   const fieldPositions = [
     {
-      name:'Goalkeeper',
+      name: 'Goalkeeper',
       primaryColor: '#FF8A29',
       secondaryColor: '#FFEEDF'
     },
     {
-      name:'Defenders',
+      name: 'Defenders',
       primaryColor: '#FFBA05',
       secondaryColor: '#FFF5D9'
     },
     {
-      name:'Midfielders',
+      name: 'Midfielders',
       primaryColor: '#57C278',
       secondaryColor: '#D9F7E9'
     },
@@ -30,23 +31,25 @@ function App() {
 
   const [players, setPlayers] = useState([])
 
-  const newPlayerAdded = (player)=>{
+  const newPlayerAdded = (player) => {
     setPlayers([...players, player])
   }
 
   return (
     <div className="App">
       <Banner />
-      <Form positions={fieldPositions.map(position => position.name)} playerInserted={player => newPlayerAdded(player)}/>
+      <div className='content'>
+        <Form positions={fieldPositions.map(position => position.name)} playerInserted={player => newPlayerAdded(player)} />
 
-      {fieldPositions.map(position => <FieldPositions 
-        key={position.name} 
-        position={position.name} 
-        backgroundColor={position.secondaryColor} 
-        cardColor={position.primaryColor}
-        players={players.filter(player => player.position === position.name)}
-      />)}
+        {fieldPositions.map(position => <FieldPositions
+          key={position.name}
+          position={position.name}
+          backgroundColor={position.secondaryColor}
+          cardColor={position.primaryColor}
+          players={players.filter(player => player.position === position.name)}
+        />)}
 
+      </div>
     </div>
   );
 }
